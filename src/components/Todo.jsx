3c291f5e9e805c1922c3482753428ad6eea8ea37
Harsh Singh <harshsingh15dec@gmail.com>
2025-08-05 01:48:38 +0530

    bug fixed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,21 +12,21 @@ const Todo = () => {
 
     const handleAdd = () =>{
         if (!task.trim()) return;
-    };
 
-    const newTodo = {
-        id: Date.now(),
-        text: task,
-    };
+        const newTodo = {
+         id: Date.now(),
+         text: task,
+        };
 
-    setTodos([...todos, newTodo]);
-    setTask('');
+        setTodos([...todos, newTodo]);
+        setTask('');
+    };
 
 
   return (
     <div className='max-w-xl mx-auto mt-10 p-6 bg-white rounded-xl shadow-lg'>
 
-        <h1 className=''>
+        <h1 className='text-2xl font-bold mb-4 text-center'>
             Todo App
         </h1>
         
@@ -39,13 +39,13 @@ const Todo = () => {
             className='flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
 
             <button 
-            onclick={handleAdd}
+            onClick={handleAdd}
             className='bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition' >Add</button>
         </div>
 
         <ul className='mt-6 space-y-3'>
             {todos.map((todo) => (
-                <li key={todo.id} className='bg-gray-100 px-4 py-2 rounded-md shadows-sm text-gray-800' >{todo.list}</li>
+                <li key={todo.id} className='bg-gray-100 px-4 py-2 rounded-md shadow-sm text-gray-800' >{todo.text}</li>
             ))}
 
         </ul>
