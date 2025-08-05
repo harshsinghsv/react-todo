9833e6652fbbbd01071797bf72f0bc657e497ac4
Harsh Singh <harshsingh15dec@gmail.com>
2025-08-05 12:17:02 +0530

    ui-improvement

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -57,35 +57,35 @@ const Todo = () => {
             Todo App
         </h1>
         
-        <div className='flex gap-3'>
+        <div className='flex gap-3 mt-4'>
             <input 
             type="text"
             placeholder="What do you ned to do ?"
             value={task}
             onChange={handleChange}
-            className='flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' />
+            className='flex-1 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm transition' />
 
             <button 
             onClick={handleAdd}
-            className='bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition' >Add</button>
+            className='bg-blue-600 text-white px-5 py-3 rounded-full hover:bg-blue-700 transition' >Add</button>
         </div>
 
         <ul className='mt-6 space-y-3'>
             {todos.map((todo) => (
-                <li key={todo.id} className='flex justify-between items-center bg-gray-100 px-4 py-2 rounded-md shadow-sm text-gray-800' >
-                    <div className='flex items-center gap-2'>
+                <li key={todo.id} className='flex justify-between items-center bg-gray-100 px-5 py-3 rounded-lg shadow-sm border border-gray-200 text-gray-800 hover:shadow-md transition' >
+                    <div className='flex items-center gap-3'>
                         <input 
                         type="checkbox" 
                         checked={todo.completed}
                         onChange={()=> handleToggle(todo.id)}
-                        className='w-4 h-4 accent-blue-600'
+                        className='w-5 h-5 accent-blue-600'
                         />
                         {editingId === todo.id ? (
                             <>
                             <input 
                             value={editedText}
                             onChange={(e)=> setEditedText(e.target.value)}
-                            className='border px-2 py-1 rounded-md text-shadow-amber-200'
+                            className='border px-2 py-1 rounded-md shadow-sm focus:ring-1 focus:ring-blue-400 text-shadow-amber-200'
                             />
                             <button
                             onClick={()=> handleSave(todo.id)}
@@ -96,7 +96,7 @@ const Todo = () => {
                             </>
 
                         ) : (
-                            <span className={todo.completed ? "line-through text-gray-500" : ""}>
+                            <span className={`text-base ${todo.completed ? "line-through text-gray-500" : "text-gray-800"}`}>
                                 {todo.text}
                             </span>
                             
@@ -104,12 +104,14 @@ const Todo = () => {
 
                     </div>
 
+
+                    <div className='flex gap-2 text-lg'>
                     <button
                     onClick={()=> {
                         setEditingId(todo.id);
                         setEditedText(todo.text);
                     }}
-                    className='text-yellow-500 hover:text-yellow-700 text-sm font-medium ml-2'
+                    className='text-yellow-500 hover:text-yellow-600 transition'
                     >
                         ✏️
                     </button>
@@ -119,10 +121,11 @@ const Todo = () => {
 
                     <button
                     onClick={()=> handleDelete(todo.id)}
-                    className='text-red-500 hover:text-red-700  text-sm font-medium'
+                    className='text-red-500 hover:text-red-600 transition'
                     >
                         ❌
                     </button>
+                    </div>
                 </li>
             ))}
 
