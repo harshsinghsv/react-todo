524809453a2b00fd844c4dd22afe17523aed47aa
Harsh Singh <harshsingh15dec@gmail.com>
2025-08-05 21:02:04 +0530

    cursor effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Todo from './components/Todo'
+import CursorEffect from './components/CursorEffect'
 
 const App = () => {
   return (
     <div>
+      <CursorEffect />
       <Todo />
     </div>
   )
